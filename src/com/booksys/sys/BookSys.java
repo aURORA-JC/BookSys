9870613719397d170ac7b5abc0baa79c680607e3
aURORA-JC <2562066819@qq.com>
2020-12-15 00:13:09 +0800

    create login ui & message

--- src/com/booksys/sys/BookSys.java
+++ src/com/booksys/sys/BookSys.java
@@ -8,35 +8,28 @@
 
 package com.booksys.sys;
 
+import java.awt.EventQueue;
+
 import com.booksys.io.IOStream;
 import com.booksys.storage.LinkedList;
+import com.booksys.ui.LoginFrame;
+import com.booksys.utils.Utils;
 
 public class BookSys {
-	static LinkedList boo = new LinkedList();
-	static LinkedList rea = new LinkedList();
-	static LinkedList bor = new LinkedList();
-	
-	public static void initData() {	
-		IOStream.bookReader(boo);
-		IOStream.readerReader(rea);
-		IOStream.borrowReader(bor);
-		loginPage();
-	}
-	
-	public static void loginPage() {
-		System.out.println("██╗     ██╗██████╗ ██████╗  █████╗ ██████╗ ██╗   ██╗██████╗");
-		System.out.println("██║     ██║██╔══██╗██╔══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝╚════██╗");
-		System.out.println("██║     ██║██████╔╝██████╔╝███████║██████╔╝ ╚████╔╝   ▄███╔╝");
-		System.out.println("██║     ██║██╔══██╗██╔══██╗██╔══██║██╔══██╗  ╚██╔╝    ▀▀══╝ ");
-		System.out.println("███████╗██║██████╔╝██║  ██║██║  ██║██║  ██║   ██║     ██╗   ");
-		System.out.println("╚══════╝╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝     ╚═╝   ");
-		System.out.println("=============================================================");
-		System.out.println("欢迎使用图书管理系统，Version 0.8.1");
-	}
-	
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		initData();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginFrame frame = new LoginFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
-	
 	
 }
